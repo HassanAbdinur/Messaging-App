d725298aa0f1166c6073e4ceaac919013ffeca59
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-10-16 15:04:00 -0700

    webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    // 1
+    
     entry: './src/index.js',
-    // 2
+    
     output: {
         path: __dirname + '/dist',
         publicPath: '/',
         filename: bundle.js
     },
-    // 3
+
     devServer: {
         contentBase: './dist'
     }
