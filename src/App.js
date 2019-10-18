9ea084b4c881da97d075578ece44117b40b5acd1
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-10-17 21:02:18 -0700

    updating component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
-class App extends React.Component {
-    render() {
+class App extends Component {
+    render () {
         return (
             <div>
                 <h1>Chat App</h1>
             </div>
         );
     }
-};
+}
+
+export default App;
