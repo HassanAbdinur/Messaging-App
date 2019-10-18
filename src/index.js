a47a73ffc369ffa4efd7a01970edadb4497201a7
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-10-17 20:15:59 -0700

    updating component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "./style/main.less";
-
-class App extends React.Component {
-    render () {
-        return (
-            <h1>Hello World</h1>
-        );
-    }
-}
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
