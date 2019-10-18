c8f478b64cca9741b45c1d9d091b37475809066a
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-10-17 20:11:42 -0700

    new component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Chat App</h1>
+            </div>
+        );
+    }
+};
