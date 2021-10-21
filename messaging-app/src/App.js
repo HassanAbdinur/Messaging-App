e4c82dc025e7a9e0642accad2496e06c8a13bf08
Hassan Abdinur <hassan.abdinur123@gmail.com>
2021-10-20 23:27:35 -0700

    making changes

--- messaging-app/src/App.js
+++ messaging-app/src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
+import { ReactDOM } from "react";
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello World
+        Hello World!
       </div>
     );
   }
 }
 
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
 export default App;
+
