97d8739ca1a330b8b2508efa4da5a8b9c74313d3
Hassan Abdinur <hassan.abdinur123@gmail.com>
2021-10-21 23:39:20 -0700

    creating class component

--- messaging-ui/src/App.js
+++ messaging-ui/src/App.js
@@ -1,15 +1,8 @@
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <h1>
-            Hello World!
-          </h1>
-      </header>
-    </div>
-  );
-}
+
+
+
 
 export default App;
