e6d7f5553772f8b07a31b25e667450fd64bd820b
Hassan Abdinur <hassan.abdinur123@gmail.com>
2021-10-21 23:59:09 -0700

    new file path

---
+++ messaging-ui/src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class Header extends React.Component {
+    render() {
+        return(
+            <div className="header-main">
+                <h1 className="header">
+                    Test
+                </h1>
+            </div>
+        );
+    }
+}
+
+export default Header;
