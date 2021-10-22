dfe64fa7abfdb736b6f3ca6b7638738bd29b0990
Hassan Abdinur <hassan.abdinur123@gmail.com>
2021-10-22 00:11:36 -0700

    rewriting class component

--- messaging-ui/src/App.js
+++ messaging-ui/src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
 import React, { Component } from 'react';
+import Header from './components/Header/Header.js';
 
-
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
 
 
 
