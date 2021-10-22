dbcf2a83f8f04f3f56aadc479807e8deb1c8c5b9
Hassan Abdinur <hassan.abdinur123@gmail.com>
2021-10-21 23:49:58 -0700

    adding title

--- messaging-ui/components/Header/header.js
+++ messaging-ui/components/Header/header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
+
 class Header extends React.Component {
     render() {
         return(
             <div className="header-main">
                 <h1 className="header">
-                    
+                    Test
                 </h1>
             </div>
         );
